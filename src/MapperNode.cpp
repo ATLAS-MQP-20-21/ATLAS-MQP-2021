fb3634f42d2fcae8fdde02ac705d526981366c36
rfcarnemolla <rfcarnemolla@wpi.edu>
2021-03-06 15:54:27 -0500

    mapper node running but not tested or even done at all

--- src/MapperNode.cpp
+++ src/MapperNode.cpp
@@ -9,6 +9,8 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Transform.h"
 #include "sensor_msgs/PointCloud2.h"
+#include <vector>
+
 //find all includes (sensor messages as well)
 
 
@@ -21,8 +23,9 @@ ros::Subscriber pointcloud_listener;
 
 
 
-void combineClouds(const sensor_msgs::PointCloud2& newcloud) {
+void addClouds(const sensor_msgs::PointCloud2& newcloud) {
     // do stuff when you get info about robot movement
+    pointcloud_publisher.publish(newcloud);
     
 }
 
@@ -31,12 +34,15 @@ int main(int argc, char *argv[])
 {
     ROS_INFO_STREAM("MapperNode");
 
-    ros::init(argc, argv, "pointcloud_transformer");
+    ros::init(argc, argv, "mapper_node");
+
+//number one victory royale 
+//yeah fortnite we bout to get down (get down!)
 
     ros::NodeHandle n;
-    //pointcloud_publisher = n.advertise<sensor_msgs::PointCloud2>("transformed_cloud", 1000); 
+    pointcloud_publisher = n.advertise<sensor_msgs::PointCloud2>("cloud_in", 1000); 
 
-    pointcloud_listener = n.subscribe("transformed_cloud", 1000, combineClouds);
+    pointcloud_listener = n.subscribe("transformed_cloud", 1000, addClouds);
 
     ROS_INFO_STREAM("Starting Spin");
     ros::spin();
